87397d7c4b014aff896b844d84c4432125c946bd
nono <np@laquadrature.net>
2021-03-02 16:33:16 +0100

    Suppression de l'applet IRC du JS

--- init.js
+++ init.js
@@ -6,23 +6,23 @@ onload = function ()
 	menu = document.createElement("menu");
 	menu.id = "menu";
 	menu.path= "";
-	
+
 	// Crée le titre du menu
 	menu.tag = document.createElement("div");
 	menu.tag.classList.add("main_title");
-	
-	menu.img = document.createElement("img");	
+
+	menu.img = document.createElement("img");
 	menu.img.src = "lqdn.svg";
 	menu.tag.appendChild(menu.img);
-	
+
 	menu.h1 = document.createElement("h1");
 	menu.h1.innerHTML = "Le carré";
 	menu.tag.appendChild(menu.h1);
-	
+
 	menu.appendChild(menu.tag);
-	
+
 	document.body.appendChild(menu);
-	
+
 	// Établie une connexion avec le serveur
 	server = new XMLHttpRequest();
 	server.open("POST", 'updateClient.php', true);
@@ -32,22 +32,19 @@ onload = function ()
 	server.send("updateClient=1");
 
 	// Lorsque la réponse à la requête est reçue
-	server.onreadystatechange = function() 
+	server.onreadystatechange = function()
 	{
-			
-		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) 
+
+		if (this.readyState == XMLHttpRequest.DONE && this.status == 200)
 		{
 			// Récupère le menu et le parse dans un tableau
 			var response = JSON.parse(server.responseText);
-		
+
 			// Construit le menu à partir de chacun de ses éléments de premier niveau
 			response.forEach(construct, menu);
-			
+
 			// Initialise le pad
 			pad.start();
-			
-			// Initialise l'applet IRC
-			irc.start();
 		}
 	}
 }
