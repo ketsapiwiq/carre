3b0fe691c12ceb3970d2240c6fc86a43a65a5636
nono <np@laquadrature.net>
2021-03-03 10:16:30 +0100

    Correction de la construction du chemin en php, suite à la correction en JS de sa création

--- admin/updateServer.php
+++ admin/updateServer.php
@@ -15,35 +15,35 @@ if (isset($_POST['updateServer']))
 {
 	// Recupère l'objet principal contenu dans le fichier
 	$content = json_decode(file_get_contents($file));
-	
+
 	// Recupère le type de la modification, spécifié par la demande
 	$type = $_POST['updateServer'];
-	
+
 	// Recupère le contenu de la modification, spécifié par la demande
 	$info = $_POST['info'];
-	
+
 	// Recupère la position de l'élément du menu à partir duquel faire la modification, spécifiée par la demande
-	$path = str_split($_POST['path']);
-	
+	$path = explode($_POST['path'], "/");
+
 	// Renomme
 	if ($type == "newName")
 		$content = newName($content, $info, $path);
-	
+
 	// Ajoute
 	else if ($type == "newPage" || $type == "newFolder")
 		$content = addElement($content, $info, $path, $type);
-	
+
 	// Déplace
 	else if ($type == "move")
 		$content = moveElement($content, $info, $path);
-	
+
 	// Supprime
 	else if ($type == "delete")
 		$content = deleteElement($content, $path);
-	
+
 	// Met à jour l'objet principal du fichier
 	file_put_contents ($file, json_encode($content));
-	
+
 	// Envoie au client le contenu du tableau de l'objet principal
 	echo json_encode($content->content);
 }
@@ -56,30 +56,30 @@ function createElement($title, $type)
 {
 	// Crée un nouvel élément
 	$element = new stdClass;
-	
+
 	// Donne au nouvel élément le titre celui indiquée par la demande
 	$element->title = $title;
-	
+
 	// Donne au nouvel élément un ID aléatoire
 	$element->id = rand(10000000, 99999999);
-	
+
 	// Si la demande concerne l'ajout d'une page
 	if ($type == "newPage")
 	{
 		// Donne à l'élément le type "page"
 		$element->type = "page";
 	}
-	
+
 	// Si la demande concerne l'ajout d'un dossier
 	else if ($type == "newFolder")
 	{
 		// Donne à l'élément le type "folder"
 		$element->type = "folder";
-		
+
 		// Donne à l'élément un tableau qui contiendra ses sous-éléments
 		$element->content = [];
 	}
-	
+
 	return $element;
 }
 
@@ -119,10 +119,10 @@ function newName($content, $info, $path)
 {
 	// Recupère l'élément
 	$element = getElement($content, $path);
-	
+
 	// Donne à l'élément le titre indiqué
 	$element->title = $info;
-	
+
 	// Renvoie l'objet principal modifié
 	return $content;
 }
@@ -135,24 +135,24 @@ function insertElement($content, $path, $element)
 {
 	// Recupère l'élément cliqué (en fonction duquel le nouvel élément doit être ajouté)
 	$syb = getElement($content, $path);
-	
+
 	// Si l'élément cliqué est un dossier
 	if ($syb->type == "folder")
 	{
 		// Ajoute le nouvel élément au début du dossier
 		array_unshift($syb->content, $element);
 	}
-	
+
 	// Si l'élément cliqué est une page
 	else if ($syb->type == "page")
 	{
 		// Recupère le dossier parent de la page cliquée
 		$parent = getParent($content, $path);
-	
+
 		// Ajoute le nouvel élément dans le dossier parent, après la page cliquée
 		array_splice( $parent->content, $path[ count($path) -1 ] +1 , 0, array ( $element ) );
 	}
-	
+
 	return $content;
 }
 
@@ -165,7 +165,7 @@ function addElement($content, $info, $path, $type)
 {
 	// Crée un nouvel élément
 	$newElement = createElement($info, $type);
-	
+
 	return insertElement($content, $path, $newElement);
 }
 
@@ -180,13 +180,13 @@ function moveElement($content, $pathTo, $path)
 
 	// Vérifie que l'élément à déplacer n'est pas un parent du dossier où on cherche à le déplacer
 	$continue = false;
-	
+
 	if ( count($pathTo) >= count($path) )
 	{
-		
+
 		for ($i = 0; $i < count($path) AND $continue == false; $i++)
 		{
-			if ( $pathTo[$i] != $path[$i] ) 
+			if ( $pathTo[$i] != $path[$i] )
 				$continue = true;
 		}
 	}
@@ -204,7 +204,7 @@ function moveElement($content, $pathTo, $path)
 		$parent = getParent($content, $path);
 
 		// Fait une cope de l'élément à déplacer
-		$elementCopy = clone $element; 
+		$elementCopy = clone $element;
 
 		// Indique que l'original de l'élément à déplacé doit être supprimé
 		$element->type = "removed";
@@ -219,7 +219,7 @@ function moveElement($content, $pathTo, $path)
 				array_splice( $parent->content, $i , 1 );
 		}
 	}
-	
+
 	return $content;
 }
 
@@ -237,10 +237,10 @@ function deleteElement($content, $path)
 
 	// Met l'élément dans le fichier des éléments supprimés
 	file_put_contents ("server/deleted.json", json_encode( $element ), FILE_APPEND );
-	
+
 	// Supprime l'élément de son parent
 	array_splice( $parent->content, $path[ count($path) -1 ] , 1 );
-	
+
 	// Renvoie l'objet principal modifié
 	return $content;
 }
