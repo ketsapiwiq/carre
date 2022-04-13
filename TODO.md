921a8a08a6585fddc4022950b6cd673402945381
Flore <paviot.adet.flore@gmail.com>
2022-04-13 17:10:35 +0200

    Affichage Menu

--- TODO.md
+++ TODO.md
@@ -1,3 +1,10 @@
 Bugs à régler : 
 
 - Affichage du logo de la Quadrature du Net
+- @important Mise à jour du pad selon l'élément cliqué dans le menu
+
+A faire : 
+
+- L'initialisation : Ajouter un pad d'accueil
+- Afficher le menu d'options
+- Possibilité d'ajouter un pad
--- static/js/init.js
+++ static/js/init.js
@@ -17,21 +17,57 @@ $(document).ready(function(){
 function init(){
     // Initialisation du pad
     pad = $("#pad");
-    pad.append("<iframe src='https://pad.lqdn.fr/p/9tlotestDev'> </iframe>")
+    pad.append("<iframe id='iPad' src='https://pad.lqdn.fr/p/9tlotestDev'> </iframe>")
     // Initialisation du menu
-    //$.getJSON('initMenu');
     // Doit récupérer le tableau contenant le menu et l'afficher sur la page
     $.get('initMenu')
         .done(function(data){
             // Traitement et affichage du menu
-            //alert(data["parent"]);
-            //alert(data[1]);
-            console.log(data);
-            menu = JSON.stringify(data);
-            menu = JSON.parse(menu);
-            console.log(menu[0]);
+            menu = JSON.parse(data)
+            menuHtml = $("#menu");
+            menuHtml.append("<li>");
+            for (var i in menu){
+                menuHtml.append("<ul><h2>" + menu[i]["parent"] + "</h2></ul>");
+                for(var j in menu[i]["pads"]){
+                    //Renvoie vers une autre page avec le pad
+                    //menuHtml.append("<ul><a href='https://pad.lqdn.fr/" + menu[i]["pads"][j]["Adresse"] + "'>" + menu[i]["pads"][j]["Nom"] + "</a></ul>");
+                    menuHtml.append("<ul><a href=''>" + menu[i]["pads"][j]["Nom"] + "</a></ul>");
+                }
+            }
+            menuHtml.append("</li>")
         })
         .fail(function(){
             alert("le callback s'est mal passé");
         })
+    //Ajout des EventListener
+    try{
+        $("ul").click(function(){
+            $(this).css('background-color', 'red');
+        });
+        /*$("h1").click(function(event)
+        {
+            console.log(event.target.id); //Affiche enfantDeMaDiv
+            console.log($(this)); //Affiche maDiv
+        });*/
+
+       // const collection = document.getElementsByTagName("ul");
+
+        /*for (let item of collection) {
+            alert("Boucle collections");
+            console.log(item.id);
+        }*/
+    } catch(err){
+        console.error(err);
+    }
+}
+
+function updateIFrame(){
+    // Supprime l'iFrame
+    document.getElementById("iPad").remove();
+    let text = $(this).innerText;
+    console.log($(this));
+    console.log("Changement d'IFrame");
+    console.log(text);
+    // Le remet
+    //pad.append("<iframe src='https://pad.lqdn.fr/" + this.Adresse + "'> </iframe>")
 }
