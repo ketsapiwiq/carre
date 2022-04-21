a2c490ac62bef36dec845dd093a288600df78a54
nono <np@laquadrature.net>
2022-04-21 12:21:03 +0200

    Le fichier d'installation à été simplifié :)

--- install.sh
+++ install.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-# 1er param : nom du dossier dans lequel on veut installer le projet
-    #if [ ! -d "$1" ] 
-    #then
-    #    mkdir "$1"
-    #    echo "Création du fichier"
-    #else
-    #    cd "$1"
-    #fi
+# Création de l'environnement virtuel Python
+virtualenv . && echo ">> environnement virtuel créé."
+# Activation dudit environnement
+source bin/activate && echo ">> environnement virtuel activé."
 
-    #Installation des dépendances
-    fic="requirements.txt"
-    pip install virutalenv
-    virtualenv "$1"
-    #Input Field Separator
-    IFS=$'\n'
-    for ligne in $(cat $fic)
-    do
-        pip install ligne
-    done
-    #Configuration et lancement de Flask
-    export FLASK_APP=app.py
-    printenv FLASK_APP
-    export FLASK_ENV=development
-    flask run
-    xdg-open 127.0.0.1:5000/
+# Installation des dépendances
+pip install -r requirements.txt && echo ">> Dépendances installées"
 
+# Configuration et lancement de Flask
+export FLASK_APP=app.py
+export FLASK_ENV=development
+nohup flask run &!
+xdg-open http://127.0.0.1:5000
+tail -f nohup.out
