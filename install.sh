fd0886cace8fe9d7794c84ff372c9cdccffb5f10
Flore <paviot.adet.flore@gmail.com>
2022-04-21 11:56:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,28 @@
+#!/bin/bash
+
 # 1er param : nom du dossier dans lequel on veut installer le projet
-install(){
-    if[! -d $1]; 
-    then
-        mkdir $1
-    else
-        cd $1
-    fi
+    #if [ ! -d "$1" ] 
+    #then
+    #    mkdir "$1"
+    #    echo "Création du fichier"
+    #else
+    #    cd "$1"
+    #fi
 
     #Installation des dépendances
     fic="requirements.txt"
     pip install virutalenv
-    vitualenv $1
+    virtualenv "$1"
     #Input Field Separator
     IFS=$'\n'
-    
     for ligne in $(cat $fic)
     do
         pip install ligne
     done
     #Configuration et lancement de Flask
     export FLASK_APP=app.py
+    printenv FLASK_APP
     export FLASK_ENV=development
     flask run
-    xdg-open 127.0.0.1:5000/ 
-}
+    xdg-open 127.0.0.1:5000/
+
